0521638cf75383e9cc14d85cd067f2f336d9f870
nabhiraj <nabhiraj96@gmail.com>
2024-06-15 01:25:09 +0530

    fixing the event image dimention problem

--- ticket_creator_app/app/eventDetails.tsx
+++ ticket_creator_app/app/eventDetails.tsx
@@ -1,10 +1,9 @@
 import EventDashBoard from "@/components/EventDashBoard";
 import { useLocalSearchParams } from "expo-router";
 import { View, Text, Image, ImageBackground, StyleSheet } from "react-native";
-import backGroundImage from './../assets/images/EventImage.png';
-import coinIcon from './../assets/images/coinIcon.png'
+import backGroundImage from './../assets/images/EventImageMobile.png';
 import { useEventContext } from "@/context/useEventContext";
-import { StatusBar } from "expo-status-bar";
+
 
 export default function eventDetails() {
     let eventIdParam = useLocalSearchParams();
@@ -19,9 +18,7 @@ export default function eventDetails() {
                 source={backGroundImage}
                 style={style.backGroundStyle}>
                 <View>
-                    <View style={{ position: 'absolute', top: 0 }}>
-                        <Image source={backGroundImage} style={{ width: 400, height: 400, resizeMode: "contain" }} ></Image>
-                    </View>
+                    
                     <EventDashBoard eventInfo={eventInfo}></EventDashBoard>
                 </View>
             </ImageBackground></>
@@ -30,6 +27,6 @@ export default function eventDetails() {
 const style = StyleSheet.create({
     backGroundStyle: {
         flex: 1,
-
+        width:'100%'
     }
 });
